af3d4044b506631472525f87a1197e468933adab
Alexandr <104072970+Dennchik@users.noreply.github.com>
2026-01-22 01:03:48 +0300

    22.01.2026.v-01.03

---
+++ src/js/main.js
@@ -0,0 +1,84 @@
+import '../scss/main.scss';
+
+// import loaded from './assets/preloader.js';
+import { smoother } from './animations/animations.jsx';
+// import { initFormValidation } from './assets/validate-form.js';
+// import { dynamicAdaptive } from './assets/dynamic-adaptive.js';
+// import { anchorsSmoothScrolling } from './assets/anchors-smooth-scrolling.js';
+// import { validateChecked } from './assets/validate-checked.js';
+// import { select } from './assets/itsSelect.js';
+
+// import {
+//   animateHeader,
+//   smoothScrollTitle,
+//   fadeInItem,
+//   fadeInBlock,
+//   fadeInColumn,
+//   fadeInItemLeft,
+//   fadeInItemRight,
+// } from './animations/anime-js.jsx';
+// import {
+//   maskPhone,
+//   cookiesAccept,
+//   shadowScrollHeader,
+//   sidebarMenuHandle,
+//   // toggleModal,
+//   lineMarquee,
+// } from './layouts/layouts.js';
+
+const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+// select();
+// loaded('.preloader');
+// fadeInColumn('.its-col');
+// fadeInBlock('.its-block');
+// fadeInItem('.its-el');
+// fadeInItemLeft('.its-el-left');
+// fadeInItemRight('.its-el-right');
+// smoothScrollTitle('.el-item');
+// initFormValidation();
+// animateHeader();
+// anchorsSmoothScrolling();
+// sidebarMenuHandle();
+// dynamicAdaptive();
+// shadowScrollHeader();
+
+// toggleModal();
+if (!isMobile) {
+  smoother();
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   validateChecked();
+//   cookiesAccept('.cookies-accept', '.cookies-accept__button');
+//   lineMarquee('.running-line__marquee');
+//   maskPhone('.phone');
+// });
+
+// if (isMobile) {
+//   const subItems = document.querySelectorAll('.header__sub-item');
+
+//   subItems.forEach((subItem) => {
+//     const trigger = subItem.closest('.header__link-key');
+//     if (!trigger) return;
+
+//     trigger.addEventListener('click', () => {
+//       const hoverItem = subItem.closest('.header__menu-items');
+
+//       if (hoverItem) {
+//         const opened = hoverItem.querySelector('.hover');
+//         if (opened && opened !== subItem) {
+//           opened.classList.remove('hover');
+//         }
+//       }
+//       trigger.classList.toggle('hover');
+//     });
+//   });
+// }
+//* ----------------------------------------------------------------------------
+console.log(
+  '%c РОССИЯ ',
+  'background: blue; color: yellow; font-size: x-large; ' +
+    'border-left: 5px solid black; border-top: 30px solid white; ' +
+    'border-right: 2px solid black; border-bottom: 30px solid red;'
+);
+// //* ----------------------------------------------------------------------------
